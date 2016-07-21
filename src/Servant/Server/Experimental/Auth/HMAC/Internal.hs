3346c451050e389dd937018567fc04d53b894b15
Al Zohali <zohl@fmap.me>
2016-07-21 21:50:52 +0300

    refactoring (wip)

--- src/Servant/Server/Experimental/Auth/HMAC/Internal.hs
+++ src/Servant/Server/Experimental/Auth/HMAC/Internal.hs
@@ -21,38 +21,34 @@
 
 module Servant.Server.Experimental.Auth.HMAC.Internal where
 
-
+import Control.Applicative
+import Control.Exception (Exception)
+import Control.Monad (when)
+import Control.Monad.Catch (MonadThrow, throwM)
 import Control.Monad.IO.Class
-import Data.Serialize            (Serialize, put, get)
-import Network.Wai               (Request, requestHeaders)
-
-import qualified Data.ByteArray as BA
-import qualified Data.ByteString as BS            (length, splitAt, concat, pack)
-import qualified Data.ByteString.Base64 as Base64 (encode, decode)
-import qualified Data.ByteString.Char8 as BS8
-
-import Data.ByteString              (ByteString)
-import Data.ByteString.Lazy         (toStrict, fromStrict)
-import Data.ByteString.Lazy.Builder (toLazyByteString)
-
-import GHC.TypeLits (Symbol)
-
-import Servant                          (throwError)
-import Servant                          (addHeader, Proxy(..))
-import Servant.API.Experimental.Auth    (AuthProtect)
-import Servant.API.ResponseHeaders      (AddHeader)
-import Servant.Server                   (ServantErr(..), err401, err403, errBody, Handler)
-import Servant.Server.Experimental.Auth (AuthHandler, AuthServerData, mkAuthHandler)
-
-import Network.HTTP.Types.Header        (hWWWAuthenticate, hAuthorization)
-import Data.Time.Clock                  (UTCTime, getCurrentTime, addUTCTime)
-import Control.Monad                    (when)
-import Data.Maybe                       (isNothing, fromJust)
-
-import Prelude hiding (takeWhile)
 import Data.Attoparsec.ByteString
 import Data.Attoparsec.ByteString.Char8 (char)
-import Control.Applicative
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy (toStrict, fromStrict)
+import Data.ByteString.Lazy.Builder (toLazyByteString)
+import Data.Maybe (isNothing, fromJust)
+import Data.Serialize (Serialize, put, get)
+import Data.Time.Clock (UTCTime, getCurrentTime, addUTCTime)
+import Data.Typeable
+import GHC.TypeLits (Symbol)
+import Network.HTTP.Types.Header (hWWWAuthenticate, hAuthorization)
+import Network.Wai (Request, requestHeaders)
+import Prelude hiding (takeWhile)
+import Servant (addHeader, Proxy(..))
+import Servant (throwError)
+import Servant.API.Experimental.Auth (AuthProtect)
+import Servant.API.ResponseHeaders (AddHeader)
+import Servant.Server (ServantErr(..), err401, err403, errBody, Handler)
+import Servant.Server.Experimental.Auth (AuthHandler, AuthServerData, mkAuthHandler)
+import qualified Data.ByteArray as BA
+import qualified Data.ByteString as BS (length, splitAt, concat, pack)
+import qualified Data.ByteString.Base64 as Base64 (encode, decode)
+import qualified Data.ByteString.Char8 as BS8
 
 
 -- | A type family that maps user-defined account type to
@@ -96,29 +92,12 @@ defaultSettings = Settings {
   , errorMessage = Nothing
   }
 
+data AuthHmacException
+  = NotAuthoirized
+  | NotAccepted ByteString
+    deriving (Eq, Show, Typeable)
 
-errNotAuthorized :: ServantErr
-errNotAuthorized = err401 {
-    errHeaders = [(hWWWAuthenticate, "HMAC realm=\"TODO\"")]
-  }
-
-errNotAccepted :: ServantErr
-errNotAccepted = err403
-
-errNoHeader :: String
-errNoHeader = "authorization header was not found"
-
-errBadHeader :: String
-errBadHeader = "malformed authorization header"
-
-errNoAccount :: String
-errNoAccount = "account was not found"
-
-errBadHash :: String
-errBadHash = "malformed hash of the request"
-
-errExpired :: String
-errExpired = "expired request"
+instance (Exception AuthHmacException)
 
 
 parseAuthentication :: Settings -> ByteString -> Maybe (Realm, ByteString, AuthHmacAccount, UTCTime)
@@ -168,16 +147,18 @@ defaultAuthHandler settings@(Settings {..}) = mkAuthHandler handler where
 
   handler :: Request -> Handler AuthHmacData
   handler req = do
+    -- TODO catch exceptions
+    result <- handler' req
+    return result
+
+  handler' :: (MonadThrow m, MonadIO m) => Request -> m AuthHmacData
+  handler' req = do
     -- TODO CI-lookup
     let authHeader = lookup hAuthorization (requestHeaders req)
-    when (isNothing authHeader) $ throwError errNotAuthorized {
-        errBody = fromStrict $ BS8.pack $ maybe errNoHeader id errorMessage
-      }
+    when (isNothing authHeader) $ throwM NotAuthoirized
 
     let authData = parseAuthentication settings $ fromJust authHeader
-    when (isNothing authData) $ throwError errNotAccepted {
-        errBody = fromStrict $ BS8.pack $ maybe errBadHeader id errorMessage
-      }
+    when (isNothing authData) $ throwM (NotAccepted "bad header")
 
     let (realm, reqHash, account, timestamp) = fromJust authData
 
@@ -185,18 +166,12 @@ defaultAuthHandler settings@(Settings {..}) = mkAuthHandler handler where
 
     let expirationTime = addUTCTime (fromIntegral maxAge) timestamp
     isExpired <- liftIO $ (expirationTime >) <$> getCurrentTime
-    when (isExpired) $ throwError errNotAccepted {
-        errBody = fromStrict $ BS8.pack $ maybe errExpired id errorMessage
-      }
+    when (isExpired) $ throwM (NotAccepted "expired request")
 
     session <- liftIO $ getSession account
-    when (isNothing session) $ throwError errNotAccepted {
-        errBody = fromStrict $ BS8.pack $ maybe errNoAccount id errorMessage
-      }
+    when (isNothing session) $ throwM (NotAccepted "account not found")
 
     let isCorrect = checkHash settings reqHash req account timestamp
-    when (not isCorrect) $ throwError errNotAccepted {
-        errBody = fromStrict $ BS8.pack $ maybe errBadHash id errorMessage
-      }
+    when (not isCorrect) $ throwM (NotAccepted "bad hash")
 
     return (account, fromJust session)
