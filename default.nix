961f09af15b771618fe4e600ee97da54765b48f0
Al Zohali <zohl@fmap.me>
2016-08-22 09:34:09 +0300

    fixup

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
 { mkDerivation, aeson, attoparsec, base, base64-bytestring
 , blaze-html, blaze-markup, bytestring, case-insensitive, cereal
-, containers, cryptonite, data-default, deepseq, exceptions
-, hspec-wai, hspec-wai-json, hspec2, http-media, http-types, memory
-, mtl, QuickCheck, random, servant, servant-blaze, servant-server
-, stdenv, string-class, text, time, transformers, unix, wai, warp
+, containers, cryptonite, data-default, exceptions, hspec
+, hspec-expectations, hspec-wai, http-media, http-types, memory
+, mtl, random, servant, servant-blaze, servant-server, stdenv
+, string-class, text, time, transformers, unix, wai, wai-extra
+, warp, with-location
 }:
 mkDerivation {
   pname = "servant-auth-hmac";
@@ -14,7 +15,7 @@ mkDerivation {
   libraryHaskellDepends = [
     attoparsec base base64-bytestring bytestring case-insensitive
     cereal cryptonite data-default exceptions http-types memory servant
-    servant-server string-class time wai
+    servant-server string-class time transformers wai
   ];
   executableHaskellDepends = [
     aeson base blaze-html blaze-markup bytestring cereal containers
@@ -22,8 +23,10 @@ mkDerivation {
     servant-server string-class text transformers unix wai warp
   ];
   testHaskellDepends = [
-    base bytestring cereal cryptonite deepseq hspec-wai hspec-wai-json
-    hspec2 QuickCheck servant-server time
+    aeson base base64-bytestring bytestring case-insensitive cereal
+    containers cryptonite data-default hspec hspec-expectations
+    hspec-wai http-types random servant servant-server string-class
+    time transformers wai wai-extra with-location
   ];
   license = stdenv.lib.licenses.gpl3;
 }
